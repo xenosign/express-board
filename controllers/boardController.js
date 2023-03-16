4ebb953fb051a75f0a62dcdeb3e5571549a1630b
xenosign <xenosign@naver.com>
2023-03-16 16:40:36 +0900

    230316 수업 코드

---
+++ controllers/boardController.js
@@ -0,0 +1,54 @@
+const connection = require('./dbConnect');
+
+const boardDB = {
+  // 모든 게시글 가져오기
+  getAllArticles: async (cb) => {
+    connection.query('SELECT * from mydb1.board', (err, data) => {
+      if (err) throw err;
+      console.log(data);
+      cb(data);
+    });
+  },
+  // 게시글 추가하기
+  writeArticle: (newArticle, cb) => {
+    connection.query(
+      `INSERT INTO mydb1.board (TITLE, CONTENT) values ('${newArticle.title}', '${newArticle.content}');`,
+      (err, data) => {
+        if (err) throw err;
+        cb(data);
+      },
+    );
+  },
+  // 특정 ID 값을 가지는 게시글 찾기
+  getArticle: (id, cb) => {
+    connection.query(
+      `SELECT * FROM mydb1.board WHERE ID_PK = ${id};`,
+      (err, data) => {
+        if (err) throw err;
+        cb(data);
+      },
+    );
+  },
+  // 특정 ID를 가지는 게시글을 수정하는 컨트롤러
+  modifyArticle: (id, modifyArticle, cb) => {
+    connection.query(
+      `UPDATE mydb1.board SET TITLE = '${modifyArticle.title}', CONTENT = '${modifyArticle.content}' WHERE ID_PK = ${id};`,
+      (err, data) => {
+        if (err) throw err;
+        cb(data);
+      },
+    );
+  },
+  // 특정 ID를 가지는 게시글 삭제하기
+  deleteArticle: (id, cb) => {
+    connection.query(
+      `DELETE FROM mydb1.board WHERE ID_PK = ${id}`,
+      (err, data) => {
+        if (err) throw err;
+        cb(data);
+      },
+    );
+  },
+};
+
+module.exports = boardDB;
