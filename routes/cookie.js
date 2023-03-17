dfd65806e7466862db99eae384cc407666be2412
xenosign <xenosign@naver.com>
2023-03-17 15:44:37 +0900

    230317 수업 코드

--- routes/cookie.js
+++ routes/cookie.js
@@ -3,12 +3,14 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  res.render('cookie');
+});
+
+router.get('/cook', (req, res) => {
   res.cookie('alert', true, {
-    expires: new Date(Date.now() + 1000 * 60),
-    httpOnly: true,
+    maxAge: 1000 * 5,
   });
-  console.log(req.cookies);
-  res.render('index');
+  res.status(200).json('쿠키 굽기 성공!');
 });
 
 module.exports = router;
